30ce3a36b6a68273e78a97cc8a80aaf1a0d6dc47
murramge <dmdgi37@naver.com>
2022-09-19 17:41:34 +0900

    프로젝트 초기설정

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './app.css';
+import Habit from './components/habit';
+
+function App() {
+  return <Habit/>;
+}
+
+export default App;
---
+++ src/components/habit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Habit extends Component {
+    render() {
+        return (
+            <div>
+               habit 
+            </div>
+        );
+    }
+}
+
+export default Habit;
