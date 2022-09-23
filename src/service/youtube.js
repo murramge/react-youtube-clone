8defd5a25bfab755d95ca3f979d2075dcf71f37d
murramge <dmdgi37@naver.com>
2022-09-23 21:11:37 +0900

    api를 fetch 말고 axios를 사용했을 때

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,27 +1,35 @@
+import axios from "axios";
 class Youtube {
   constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-  }
-  async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.requestOptions
-    );
-    const result = await response.json();
-    return result.items;
+    this.youtube = axios.create({
+      baseURL: "https://youtube.googleapis.com/youtube/v3",
+      params: { key },
+    });
   }
 
+  async mostPopular() {
+    const response = await this.youtube.get("videos", {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 25,
+      },
+    });
+    return response.data.items;
+  }
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=${this.key}`,
-      this.requestOptions
-    );
-    const result = await response.json();
-    return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 25,
+        type: "video",
+        q: query,
+      },
+    });
+    return response.data.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
   }
 }
 
